32466ab5ed1c4b473cf65762a463290763b02e49
Ope Ajayi <105161691+Iamopekan@users.noreply.github.com>
2022-10-29 11:52:08 +0100

    Add files via upload

---
+++ (Week 5 Assignment)Django CRUD - musicapp/musicapp/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import Httpresponse
+
+# Create your views here.
+def index(request):
+    return Httpresponse('Hello, Welcome to my musicapp site')
